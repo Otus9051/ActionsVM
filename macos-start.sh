50b950791a2777022b119629debd2c950b4f39a0
Otus9051 <58171861+Otus9051@users.noreply.github.com>
2021-07-27 17:51:57 +0530

    Update macos-start.sh

--- macos-start.sh
+++ macos-start.sh
@@ -7,8 +7,4 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
  -configure -access -on -privs -all -users root
 
-PDISK=$(diskutil list physical external | head -n1 | cut -d" " -f1)
-APFSCONT=$(diskutil list physical external | grep "Apple_APFS" | tr -s " " | cut -d" " -f8)
-sudo diskutil repairDisk $PDISK
-sudo diskutil apfs resizeContainer $APFSCONT 0
 brew tap jakehilborn/jakehilborn && brew install displayplacer
