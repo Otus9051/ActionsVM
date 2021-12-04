896890a3b2b5991897d344187ba34b9445947be9
Otus9051 <58171861+Otus9051@users.noreply.github.com>
2021-12-04 12:01:38 +0530

    Update macos-start.sh

--- macos-start.sh
+++ macos-start.sh
@@ -1,10 +1,1 @@
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
--activate -configure -access -on \
--configure -allowAccessFor -specifiedUsers \
--configure -users root \
--configure -restart -agent -privs -all
 
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
- -configure -access -on -privs -all -users root
-
-brew tap jakehilborn/jakehilborn && brew install displayplacer
